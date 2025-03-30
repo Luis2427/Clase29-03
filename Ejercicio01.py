b59582ee95e5b06d199985854868af27ea73858a
Luis2427 <75006556@continental.edu.pe>
2025-03-29 20:12:34 -0500

    Add files via upload

---
+++ Ejercicio01.py
@@ -0,0 +1,14 @@
+# Objetivo: Area de un rectangulo
+# Nombre: COBEÑAS BLANCO VICTOR LUIS
+# Fecha: 29/03/2025
+
+
+# ENTRADA
+base=int(input("Ingrese la base: "))
+altura=int(input("Ingrese la altura: "))
+
+# PROCESO
+Calcular = base * altura
+
+# SALIDA
+print("El area del rectangulo con los datos ingresados es: ",Calcular)
---
+++ Ejercicio02.py
@@ -0,0 +1,13 @@
+# # Objetivo: VOLUMEN DE UN CUBO
+# Nombre: COBEÑAS BLANCO VICTOR LUIS
+# Fecha: 29/03/2025
+
+
+# ENTRADA
+lado=int(input("Ingrese el lado: "))
+
+# PROCESO
+Calcular = lado ** 3
+
+# SALIDA
+print("El volumen con los datos ingresados es: ",Calcular)
